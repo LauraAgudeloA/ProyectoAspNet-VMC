e13e9c56be76ff12c9544762d9dfd16fd75d6546
Laura Agudelo <lauraagudelo102001@gmail.com>
2022-05-26 16:18:14 -0500

    Creación de proyecto Asp net core y mvc de udemy

---
+++ CrudEFCore/Datos/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEFCore.Datos
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        //Agregar todos los modelos aquí
+        //(localDb)\MSSQLLocalDB
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
